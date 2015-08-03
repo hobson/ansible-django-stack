6d55d5bc503ae622ead7299b8c18797d16b756f6
Hobson Lane <hobsonlane@gmail.com>
2015-08-03 07:55:23 -0700

    wrong zip filename and both old and new catalyst downloaded, binaries and source

--- roles/gpu/files/install_catalyst.sh
+++ roles/gpu/files/install_catalyst.sh
@@ -3,7 +3,9 @@
 
 DOWNLOAD_URL="$1"
 DOWNLOAD_PATH="$2"
+# {{ ansible_distribution }}  # e.g. "Ubuntu"
 DISTRO=$(cat /etc/debian_version)
+# {{ ansible_distribution_release }}  # e.g. "precise"
 DISTRO=${ver##*/}
 
 echo "DOWNLOAD_URL=$DOWNLOAD_URL"
@@ -14,7 +16,7 @@ cd /tmp
 # without referrer, wget will fail
 # wget --referer=http://support.amd.com http://www2.ati.com/drivers/linux-amd-14.41rc1-opencl2-sep19.zip -O install_opencl_amd_driver.zip
 wget --referer=http://support.amd.com -O "$DOWNLOAD_PATH"/install_catalyst.zip -c "$DOWNLOAD_URL"
-unzip install_catalyst.zip
+unzip -o install_catalyst.zip
 cd fglrx*
 sudo chmod +x amd-driver-installer*.run
 # For Ubuntu LightDM || Gnome GDM || Linux Mint MDM
