4ffeee19163a311176ecddbf2567b88eb0a0f943
Hobson Lane <hobsonlane@gmail.com>
2015-10-17 02:21:26 -0700

    some helper scripts for granting permissions to all tables in db restored from a dump

--- roles/db/grant_all.sh
+++ roles/db/grant_all.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env sh
-sudo su postgres
-# dropdb "${DATABASE_NAME}"
-# psql -c "CREATE USER ${DATABASE_USER};"
-# psql -c "ALTER USER ${DATABASE_USER} WITH PASSWORD '${DATABASE_PASSWORD}';"
-# createdb "$DATABASE_NAME" -O "$DATABASE_USER"
-# psql "$DATABASE_NAME" -f dump.sql
+
+# if you source this script as the postgres user and you have set
+# the DATABASE_NAME and DATABASE_USER
+# this script will grant that user permissions to all the tables/rels in that DB
+
+# sudo su postgres
+
+# if [ -n "$1" ]; then
+#    DATABASE_NAME="$1"
+# fi
+# if [ -n "$2" ]; then
+#    DATABASE_USER="$2"
+# fi
+
 for tbl in `psql -qAt -c "select tablename from pg_tables where schemaname = 'public';" $DATABASE_NAME` ; do  psql -c "alter table \"$tbl\" owner to $DATABASE_USER" $DATABASE_NAME ; done
 psql "$DATABASE_NAME" -c "GRANT ALL ON ALL TABLES IN SCHEMA public to ${DATABASE_USER};"
 for tbl in `psql -qAt -c "select sequence_name from information_schema.sequences where sequence_schema = 'public';" $DATABASE_NAME` ; do  psql -c "alter table \"$tbl\" owner to $DATABASE_USER" $DATABASE_NAME ; done
 psql "$DATABASE_NAME" -c "GRANT ALL ON ALL SEQUENCES IN SCHEMA public to ${DATABASE_USER};"
 for tbl in `psql -qAt -c "select table_name from information_schema.views where table_schema = 'public';" $DATABASE_NAME` ; do  psql -c "alter table \"$tbl\" owner to $DATABASE_USER" $DATABASE_NAME ; done
 psql "$DATABASE_NAME" -c "GRANT ALL ON ALL FUNCTIONS IN SCHEMA public to ${DATABASE_USER};"
-
-# curl https://dl.dropboxusercontent.com/u/27181407/hackoregon_dump_2015-10-02.sql | psql "${DATABASE_USER}"
